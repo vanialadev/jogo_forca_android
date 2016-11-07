4528c0ed1677a7d5e7ff52203819a544c2dc05e3
Vania Almeida <vanialadev@gmail.com>
2016-11-07 00:27:36 -0300

    desenhando retângulos

--- app/src/main/java/com/vania/jogoforca/EstudoView.java
+++ app/src/main/java/com/vania/jogoforca/EstudoView.java
@@ -2,7 +2,9 @@ package com.vania.jogoforca;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -24,6 +26,37 @@ public class EstudoView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawLine(0, 0, 100, 100, new Paint());
+        desenhaLinha(canvas);
+
+        desenhaRetangulo(canvas);
     }
+
+    private void desenhaRetangulo(Canvas canvas) {
+        canvas.drawRect(new Rect(200, 200, 300, 300), new Paint());
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+
+        canvas.drawLine(200, 200,300,300, paint);
+
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+
+        for (int i = 1; i <= 20; i++){
+            canvas.drawRect(300 + (i * 20 * -1), 300 + (i * 20 * -1), 400 + (i * 20), 400 + (i * 20), paint);
+        }
+    }
+
+    private void desenhaLinha(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setStrokeWidth(0);
+        paint.setColor(Color.RED);
+
+        for (int n = 1; n <= 20; n++){
+            paint.setStrokeWidth(n);
+            canvas.drawLine(50, n*20, 100, n*20, paint);
+        }
+    }
+
+
 }
