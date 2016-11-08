7455b4d83668a9b9ccb41653ae4cef73efb66886
Vania Almeida <vanialadev@gmail.com>
2016-11-08 02:25:17 -0300

    criando botões e passando eles pra activity

--- app/src/main/java/com/vania/jogoforca/MainActivity.java
+++ app/src/main/java/com/vania/jogoforca/MainActivity.java
@@ -2,12 +2,49 @@ package com.vania.jogoforca;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btJogar;
+    private Button btPlay;
+    private EditText etLetra;
+    private ForcaView forcaView;
+    private ForcaController forcaController;
+    private String[] palavras = new String[] {"alura", "caelum"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btJogar = (Button) findViewById(R.id.btJogar);
+        btPlay = (Button) findViewById(R.id.btJogar);
+        etLetra = (EditText) findViewById(R.id.etLetra);
+
+        forcaView = (ForcaView) findViewById(R.id.fvJogo);
+    }
+
+    private void init(){
+
+        btPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setForcaController(new ForcaController(palavras[new Random().nextInt(palavras.length)]));
+            }
+        });
+
+    }
+
+    public ForcaController getForcaController() {
+        return forcaController;
+    }
+
+    public void setForcaController(ForcaController forcaController) {
+        this.forcaController = forcaController;
     }
 }
