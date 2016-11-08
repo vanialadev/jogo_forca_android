b32859c1cf11b7ef151ace3ef3dcd7b894d45e4b
Vania Almeida <vanialadev@gmail.com>
2016-11-08 02:06:13 -0300

    criando o controller da forca

---
+++ app/src/main/java/com/vania/jogoforca/ForcaController.java
@@ -0,0 +1,66 @@
+package com.vania.jogoforca;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by vania on 08/11/16.
+ */
+
+public class ForcaController {
+
+    private String palavraParaAdvinhar;//palavra a ser advinhada
+    private Set<Character> letrasUsadas;//lista com as leras que foram usadas, sem repetir SET
+    private int qntErros = -1; //quantidade de erros
+
+    public ForcaController(String palavra) {
+        this.palavraParaAdvinhar = palavra;
+        letrasUsadas = new HashSet<>();
+    }
+
+    public void Joga(Character letra){
+        //caso o set ja tenha  aletra jogada saimos da funcao
+        if(letrasUsadas.contains(letra)){
+            return;
+        }
+        letrasUsadas.add(letra);
+        if(palavraParaAdvinhar.contains(letra.toString())){
+            return;
+        }
+        qntErros++;
+
+    }
+
+    public String getpalavraAteAgora(){
+        String visuazlizacao = "";
+        for(char c : palavraParaAdvinhar.toCharArray()){
+            if(letrasUsadas.contains(c)){
+                visuazlizacao += c;
+
+            }else {
+                visuazlizacao += " ";
+            }
+        }
+        return visuazlizacao;
+    }
+
+    public boolean isMorreu(){
+        return getQntErros() == 5;
+    }
+
+    public boolean isGanhou(){
+        return !getpalavraAteAgora().contains(" ");
+    }
+
+    public boolean isTerminou(){
+        return isGanhou() || isMorreu();
+    }
+
+    public int getQntErros() {
+        return qntErros;
+    }
+
+    public void setQntErros(int qntErros) {
+        this.qntErros = qntErros;
+    }
+}
